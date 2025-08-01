1cef7810639df23e4dda575368783e7ac9e47dec
BabyShaliniChandran <Baby.Chandran@Coforge.com>
2025-08-01 13:01:24 +0530

    version2

--- Activities/Day17/activity2.py
+++ Activities/Day17/activity2.py
@@ -3,6 +3,17 @@ n=100
 output=[]
 for i in range(2,n+1):
 	output.append(i)
+'''
+def prime_numbers():
+	result=[]
+	for i in range(2,n+1):
+		for j in output:
+			if i==j:
+				result.append(j)
+			if (j%i!=0):
+				result.append(j)
+		return result
+print(prime_numbers())
 
 
 def numbers():
@@ -15,3 +26,23 @@ def numbers():
 				result.append(j)
 		return result
 print(numbers())
+'''
+n=100
+
+output=list(range(2,n+1))
+
+def numbers(divisor_number):
+	divisor=divisor_number
+	result=[]
+	for num in output:
+		if num==divisor:
+			result.append(num)
+		if num%divisor!=0:
+			result.append(num)	
+	return result
+
+
+for i in range(2,(n+1)//2):
+	output=numbers(i)
+	print(numbers(i))
+
